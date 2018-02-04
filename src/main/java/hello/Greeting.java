e1f332f6756aa33d9bbde59a109187385e563c6f
smittty <smithwc6@live.com>
2018-02-04 14:36:24 -0500

    Updated for properties file

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,13 +1,18 @@
 package hello;
 
+import dblearn.MySqlLearn;
+
 public class Greeting {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+
+
+    public Greeting(long id, String content, MySqlLearn theDb) {
         this.id = id;
         this.content = content;
+
     }
 
     public long getId() {
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,8 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import dblearn.MySqlLearn;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,15 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    MySqlLearn theDb = new MySqlLearn();
+
+
+
+
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+                String.format(template, name),theDb);
     }
 }
